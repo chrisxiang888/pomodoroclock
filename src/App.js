54e2154674760f89402b58e23cff937bc3224802
Nan Xiang <chrisxiang86@hotmail.com>
2022-05-21 18:25:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React,{ useState } from "react";
+import breaksound from "./breaksound.mp3"
+
+function App(){
+
+  const[displayTime,setDisplayTime]=React.useState(5);
+  const[breakTime,setBreakTime]=React.useState(3)
+  const[sessionTime,setSessionTime]=React.useState(5);
+  const [timerOn,setTimerOn]=React.useState(false)
+  const[onBreak,setOnBreak]=React.useState(false)
+  const[breakAudio,setBreakAudio]=React.useState(new Audio(breaksound))
+  
+  const playBreakSound=()=>{
+      breakAudio.currentTime=0;
+      breakAudio.play();
+
+  }
+
+  const formatTime=(time)=>{
+      let minutes=Math.floor(time/60);
+      let seconds=time % 60;
+     return `${minutes<10?"0"+minutes:minutes}:${seconds<10?"0"+seconds:seconds}`
+  }
+const changeTime=(amount,type)=>{
+    if (type==='break'){
+        if(breakTime<=60 && amount<0){
+            return;
+        }
+       setBreakTime((prev)=>prev+amount);
+    }else{
+      if(sessionTime<=60 &&amount<0){
+          return;}
+      setSessionTime((prev)=>prev+amount)
+      if(!timerOn){
+          setDisplayTime(sessionTime+amount)
+      }
+    }
+
 }
 
+const controlTime=()=>{
+    let second=1000;
+    let date=new Date().getTime();
+    let nextDate=new Date().getTime()+second;
+    let onBreakVariable=onBreak;
+    if(!timerOn){
+        let interval=setInterval(()=>{
+           date=new Date().getTime();
+           if(date>nextDate){
+               setDisplayTime(prev=>{
+                   if (prev<=0 && !onBreakVariable){
+                       playBreakSound();
+                       onBreakVariable=true;
+                       setOnBreak(true);
+                       return breakTime;
+                   }else if (prev<=0&& onBreakVariable){
+                       playBreakSound();
+                       onBreakVariable=false;
+                       setOnBreak(false);
+                       return sessionTime;
+                   }
+                   return prev-1;
+               })
+               nextDate+=second;
+           }
+           
+        },30)
+        localStorage.clear()
+        localStorage.setItem('interval-id',interval)
+    }
+    if (timerOn){
+        clearInterval(localStorage.getItem("interval-id"))
+    }
+
+    setTimerOn(!timerOn)
+}
+
+const resetTime=()=>{
+    setDisplayTime(25*60);
+    setBreakTime(5*60);
+    setSessionTime(25*60)
+}
+  return(
+<div className="body">
+  <div className="center-align">
+      <h1>    Clock</h1>
+      <div class="row">
+
+          <div class="col s6">
+          <Length 
+              title={"break length"} 
+              changeTime={changeTime} 
+              type={"break"} 
+              formatTime={formatTime} 
+              time={breakTime}/>
+          </div>
+
+          <div class="col s6">
+          <Length 
+              title={"session length"} 
+              changeTime={changeTime} 
+              type={"session"} 
+              formatTime={formatTime} 
+              time={sessionTime}/>
+          </div>
+         
+          </div>
+
+       <h3>{onBreak ?"Break":"Session"}</h3>
+      <h1>{formatTime(displayTime)}</h1>
+      <button className="btn-large deep-purple lighten-2" onClick={controlTime}>
+      {timerOn ?(
+          <i className="material-icons">pause_circle_filled</i>
+      ):(
+          <i className="material-icons">play_circle_filled</i>
+      )}
+     </button>
+     <button className="btn-large deep-purple lighten-2" onClick={resetTime}>
+     <i className="material-icons">autorenew</i>
+     </button>
+  </div>
+  </div>
+  )
+}
+
+  function Length({title,changeTime,type,time,formatTime}){
+      return(
+          <div>
+              <h3>{title}</h3>
+              <div className='time-sets'>
+                  <button className="btn-small deep-purple lighten-2"
+                  onClick={()=>changeTime(-60,type)}>
+                      <i className="material-icons">arrow_downward</i>
+                  </button>
+                  <h3>{formatTime(time)}</h3>
+
+                  <button className="btn-small deep-purple lighten-2"
+                   onClick={()=>changeTime(60,type)}>
+                 
+                      <i className="material-icons">arrow_upward</i>
+                  </button> 
+                 
+              </div>
+          </div>
+      )
+  }
 export default App;
